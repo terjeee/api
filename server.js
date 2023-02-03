8d5b0fdd5e93676c01414ab20ddc9f21887bf8c9
terjeee <96795863+terjeee@users.noreply.github.com>
2023-02-03 23:36:27 +0100

    :)

---
+++ server.js
@@ -0,0 +1,8 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const app = express();
