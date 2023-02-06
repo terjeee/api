55a5812594978f848388f170a79c72a55d69407e
terjeee <96795863+terjeee@users.noreply.github.com>
2023-02-06 23:26:56 +0100

    ready, set, go

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express from "express";
+import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 
 const app = express();
+const port: Number = Number(process.env.PORT) || 3000;
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.status(200).send({ status: "success", message: "hei" });
 });
 
-const port = process.env.PORT;
 app.listen(port, () => console.log(`✅ SERVER @ https://localhost:${port}`));
