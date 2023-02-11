2f2535025ea2bc6d8775b3d73dc48a7c7169b8fd
terjeee <96795863+terjeee@users.noreply.github.com>
2023-02-11 03:34:25 +0100

    begynnelse på controllerFood

---
+++ src/controllers/controllerFood.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+
+async function getRecipes(request: Request, response: Response) {
+  console.log("getRecipes");
+  response.send({ status: "success", message: "getRecipes" });
+}
+
+async function getRecipeId(request: Request, response: Response) {
+  console.log("getRecipeId");
+  response.send({ status: "success", message: "getRecipeId" });
+}
+
+async function postRecipe(request: Request, response: Response) {
+  console.log("postRecipe");
+  response.send({ status: "success", message: "postRecipe" });
+}
+
+async function patchRecipe(request: Request, response: Response) {
+  console.log("patchRecipe");
+  response.send({ status: "success", message: "patchRecipe" });
+}
+
+export default { getRecipes, getRecipeId, postRecipe, patchRecipe };
