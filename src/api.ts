79b29d89de31c1c43f610f511d29468c129c1296
terjeee <96795863+terjeee@users.noreply.github.com>
2023-02-18 04:35:56 +0100

    byttet navn fra app -> api

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,7 @@ import morgan from "morgan";
 import routerAuth from "./routes/routerAuth";
 import routerFood from "./routes/routerFood";
 
-const app = express();
+const api = express();
 
 // ENVIRONMENT //
 // ENVIRONMENT //
@@ -12,26 +12,26 @@ const app = express();
 if (process.env.NODE_ENV.trim() === "production") console.log("✅ ENV: PRODUCTION");
 if (process.env.NODE_ENV.trim() === "development") {
   console.log("✅ ENV: DEVELOPMENT");
-  app.use(morgan("dev"));
+  api.use(morgan("dev"));
 }
 
 // MIDDLEWARE //
 // MIDDLEWARE //
 
-app.use(express.json());
+api.use(express.json());
 
 // ROUTES //
 // ROUTES //
 
-app.use("/api/v1/auth", routerAuth);
-app.use("/api/v1/food", routerFood);
-// app.use("/api/v1/projects", routerProjects )
+api.use("/api/v1/food", routerFood);
+// api.use("/api/v1/auth", routerAuth);
+// api.use("/api/v1/projects", routerProjects )
 
-app.use("*", (req, res, next) => {
+api.use("*", (req, res, next) => {
   res.status(404).send({ status: "error", message: "Endpoint doesn't exist." });
 });
 
 // ERROR //
 // ERROR //
 
-export default app;
+export default api;
