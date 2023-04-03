e9ae072e40a88ef2907cb2766c75acbd154f911d
terjeee <96795863+terjeee@users.noreply.github.com>
2023-04-03 09:13:03 +0200

    + postRecipe (90%)

--- src/controllers/controllerFood.ts
+++ src/controllers/controllerFood.ts
@@ -1,11 +1,31 @@
 import { Request, Response } from "express";
+import ModelRecipe from "../models/ModelRecipe";
 
-export async function getAllRecipes(request: Request, response: Response) {
-  console.log("getAllRecipes");
-  response.send({ status: "success", message: "getRecipes" });
+export async function postRecipe(req: Request, res: Response) {
+  const recipe = {
+    title: req.body.title,
+  };
+
+  await ModelRecipe.create(recipe);
+
+  res.status(200).send({
+    ok: true,
+    recipe: recipe,
+  });
 }
 
-export async function getRecipeById(request: Request, response: Response) {
-  console.log("getRecipeById");
-  response.send({ status: "success", message: "getRecipeId" });
+export async function getRecipes(req: Request, res: Response) {
+  res.send({
+    ok: true,
+    message: "getRecipes",
+  });
+}
+
+export async function getRecipeById(req: Request, res: Response) {
+  const param = req.params.id;
+
+  res.send({
+    ok: true,
+    message: `getRecipe${param}`,
+  });
 }
