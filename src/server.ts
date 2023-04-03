cf8f033281870501bd601a6f5a8c5ce12d09c94f
terjeee <96795863+terjeee@users.noreply.github.com>
2023-04-03 09:12:47 +0200

    connect mongoDb til app

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({ path: "./config.env" });
 
+import mongoose from "mongoose";
 import api from "./api";
 
-const port: Number = Number(process.env.PORT) || 3000;
+const port = Number(process.env.PORT) || 3000;
+const DATABASE = process.env.DATABASE;
+
+// Connect Database //
+// Connect Database //
+
+mongoose.set({ strictQuery: true });
+mongoose.connect(DATABASE, {}).then((connection) => console.log("✅ DATABASE CONNECTED"));
+
+// Init Server //
+// Init Server //
 
 api.listen(port, () => {
   console.log(`✅ API AVAILABLE @ https://localhost:${port}`);
