24d2e2ad31ed02d2640da1beaed5a2948065533e
terjeee <96795863+terjeee@users.noreply.github.com>
2023-04-04 07:11:53 +0200

    :)

--- src/controllers/controllerFood.ts
+++ src/controllers/controllerFood.ts
@@ -4,6 +4,10 @@ import ModelRecipe from "../models/ModelRecipe";
 export async function postRecipe(req: Request, res: Response) {
   const recipe = {
     title: req.body.title,
+    servings: req.body.servings,
+    duration_min: req.body.duration_min,
+    ingredients: req.body.ingredients,
+    steps: req.body.steps,
   };
 
   await ModelRecipe.create(recipe);
@@ -15,17 +19,21 @@ export async function postRecipe(req: Request, res: Response) {
 }
 
 export async function getRecipes(req: Request, res: Response) {
+  const tours = await ModelRecipe.find({ accepted: true });
+
   res.send({
     ok: true,
-    message: "getRecipes",
+    results: tours.length,
+    data: tours,
   });
 }
 
 export async function getRecipeById(req: Request, res: Response) {
   const param = req.params.id;
+  const tour = await ModelRecipe.find({ accepted: true, _id: param });
 
   res.send({
     ok: true,
-    message: `getRecipe${param}`,
+    message: tour,
   });
 }
