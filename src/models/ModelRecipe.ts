8d34d8d769a59e0d054e3190627553305c25cdf5
terjeee <96795863+terjeee@users.noreply.github.com>
2023-04-04 07:11:48 +0200

    :)

--- src/models/ModelRecipe.ts
+++ src/models/ModelRecipe.ts
@@ -1,51 +1,59 @@
 import mongoose, { Schema } from "mongoose";
 
-const ingredientSchema = new mongoose.Schema({
-  ingredient: { type: String, required: [true, "Ingredient is required :)"] },
-  quantity: { type: String, require: true },
-});
+const ingredientSchema = new mongoose.Schema(
+  {
+    ingredient: { type: String, required: [true, "Ingredient is required :)"] },
+    quantity: { type: String, require: true },
+  },
+  { _id: false }
+);
 
-const schemaRecipe: Schema = new mongoose.Schema({
-  accepted: {
-    type: Boolean,
-    default: false,
-    select: false,
-  },
-  title: {
-    type: String,
-    required: [true, "Title is required :)"],
-    unique: [true, "Recipe title already exists!"],
-  },
-  num_servings: {
-    type: String,
-    required: [true, "Servings is required :)"],
-  },
-  duration_min: {
-    type: String,
-    required: [true, "Duration is required :)"],
-  },
-  description: {
-    type: String,
-    required: false,
-  },
-  ingredients: {
-    type: [ingredientSchema],
-    required: [true, "[Ingredients] are required :)"],
-    validate: function () {
-      this.ingredients.length > 0;
+const schemaRecipe: Schema = new mongoose.Schema(
+  {
+    accepted: {
+      type: Boolean,
+      default: false,
+      select: false,
+    },
+    title: {
+      type: String,
+      required: [true, "Title is required :)"],
+      unique: [true, "Recipe title already exists!"],
+    },
+    servings: {
+      type: String,
+      required: [true, "Servings is required :)"],
+    },
+    duration_min: {
+      type: String,
+      required: [true, "Duration is required :)"],
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    ingredients: {
+      type: [ingredientSchema],
+      required: [true, "[Ingredients] are required :)"],
+      validate: function () {
+        this.ingredients.length > 0;
+      },
+    },
+    steps: {
+      type: [{ type: String }],
+      required: [true, "[Steps] are required :)"],
+    },
+    created_at: {
+      type: Date,
+      default: Date.now(),
+    },
+    updated_at: {
+      type: Date,
     },
   },
-  steps: {
-    type: [{ type: String }],
-    required: [true, "[Steps] are required :)"],
-  },
-  created_at: {
-    type: Date,
-    default: Date.now(),
-  },
-  updated_at: {
-    type: Date,
-  },
-});
+  {
+    versionKey: false,
+  }
+);
 
 export default mongoose.model("Recipe", schemaRecipe);
